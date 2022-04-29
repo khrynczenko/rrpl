572a2a5325c88dbf8085e7913d0c5b7d8332c5d3
Krzysztof Hrynczenko <jeniopy@gmail.com>
2022-04-29 18:21:41 +0200

    fix: print errors to stderr instead of stdout

--- src/io.rs
+++ src/io.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 pub fn read_file(path: &Path) -> String {
     let content = fs::read_to_string(path);
     if let Err(e) = content {
-        println!("{}", e);
+        eprintln!("{}", e);
         match e.raw_os_error() {
             Some(code) => std::process::exit(code),
             None => std::process::exit(1),
@@ -20,7 +20,7 @@ pub fn write_file(path: &Path, new_content: &str) {
     let file = File::create(path);
 
     if let Err(e) = file {
-        println!("{}", e);
+        eprintln!("{}", e);
         match e.raw_os_error() {
             Some(code) => std::process::exit(code),
             None => std::process::exit(1),
@@ -30,7 +30,7 @@ pub fn write_file(path: &Path, new_content: &str) {
     let mut file = file.unwrap();
 
     if let Err(e) = file.write_all(new_content.as_bytes()) {
-        println!("{}", e);
+        eprintln!("{}", e);
         match e.raw_os_error() {
             Some(code) => std::process::exit(code),
             None => std::process::exit(1),
